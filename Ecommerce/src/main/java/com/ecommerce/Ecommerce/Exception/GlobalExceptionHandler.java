3f602de1b70bbf63f30cac42bcb8847c8772fa4f
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-06-04 14:21:23 +0530

    Brush up

--- Ecommerce/src/main/java/com/ecommerce/Ecommerce/Exception/GlobalExceptionHandler.java
+++ Ecommerce/src/main/java/com/ecommerce/Ecommerce/Exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
